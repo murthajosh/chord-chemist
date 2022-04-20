2ea0f246653155d80aa9cf363f63ff513caf151d
Josh Murtha <murthajosh@gmail.com>
2022-04-20 15:05:42 -0500

    got the value of the select stored in a variable and have it alerting on change. Next I'll use it to activate the right fret circles.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import Fretboard from './components/Fretboard'
 import axios from 'axios'
 import { BASE_URL} from './globals'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import ChordPickerForm from './components/ChordPickerForm';
 
 
 function App() {
+  // const [chord, setChord] = useState
 
   useEffect(() => {
     const getChords = async () => {
--- src/components/ChordPickerForm.jsx
+++ src/components/ChordPickerForm.jsx
@@ -1,11 +1,20 @@
-const ChordPickerForm = () => {
+const ChordPickerForm = (props) => {
+
+    const setChord = () => {
+        const chord = document.getElementById('root-select').value
+        alert(chord)
+    }
+
+    const chordPicker = document.getElementById('root-select')
 
     return (
-        <form className="chord-picker"> 
-            <label htmlFor="root-select">Choose a Root:</label>
 
-            <select name="roots" id="root-select">
-                <option value="">--Choose a Root--</option>
+        <form className="chord-picker">
+
+            <label htmlFor="root-select">Choose a Chord:</label>
+
+            <select name="roots" id="root-select" onChange={setChord}>
+                <option value="C">--Choose a Chord--</option>
                 <option value="a">A</option>
                 <option value="a#">A#</option>
                 <option value="b">B</option>
@@ -20,15 +29,6 @@ const ChordPickerForm = () => {
                 <option value="g#">G#</option>
             </select>  
 
-            <label htmlFor="quality-select">Choose a Chord Quality:</label>
-
-            <select name="roots" id="root-select">
-                <option value="">--Choose a Quality--</option>
-                <option value="major">Major</option>
-                <option value="minor">Minor</option>
-            </select>   
-
-            <button className="submit">Submit</button>         
         </form>
     )
 }

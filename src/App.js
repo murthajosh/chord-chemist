f4446ac90f73dbd1ec46e81961d3cbfe3728cc86
Josh Murtha <murthajosh@gmail.com>
2022-04-20 14:07:21 -0500

    got api data coming in and logging to the console.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import './App.css';
 import Fretboard from './components/Fretboard'
+import axios from 'axios'
+import { BASE_URL} from './globals'
+import { useEffect } from 'react'
 
 
 function App() {
+
+  useEffect(() => {
+    const getChords = async () => {
+      const response = await axios.get(`${BASE_URL}/chords/F_maj7`)
+      console.log(response)
+    }
+    getChords()
+  }, [])
+  
   return (
     <div className="App">
       <Fretboard />

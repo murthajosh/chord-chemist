9150265db8eda5dca8671049adfd9497382a1841
Josh Murtha <murthajosh@gmail.com>
2022-05-01 14:24:39 -0500

    i think firebase hosting is set up now.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCuRB3rlJLtTD-8c_aU9uyehTM8IPCYH4",
+  authDomain: "chord-chemist.firebaseapp.com",
+  projectId: "chord-chemist",
+  storageBucket: "chord-chemist.appspot.com",
+  messagingSenderId: "624842935143",
+  appId: "1:624842935143:web:6af1f67a95e2b5f5b43dc2"
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
